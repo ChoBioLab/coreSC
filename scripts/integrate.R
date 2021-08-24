73921e65759423d882dfa31cfec09dd12d130f41
Christopher Tastad <ctastad@gmail.com>
2021-08-24 15:22:06 -0500

    removed uncessary loop from integrate process

--- scripts/integrate.R
+++ scripts/integrate.R
@@ -6,46 +6,44 @@ library(dplyr)
 load("./tmp/base_image.RData")
 objects <- read_object("individual_objects")
 
-for (i in 1:nrow(samples)) {
-  d <- params["dims", ]
-  x <- FindIntegrationAnchors(
-    object.list = objects,
-    dims = 1:d
-  )
-  x <- IntegrateData(
-    anchorset = x,
-    dims = 1:d
-  )
-  DefaultAssay(x) <- "integrated"
-  str_section_head("Integrated") # logging
-  all.genes <- rownames(x)
-  x <- ScaleData(
-    x,
-    verbose = FALSE,
-    features = all.genes
-  )
-  x <- RunPCA(
-    x,
-    npcs = d,
-    verbose = FALSE
-  )
-  x <- RunUMAP(
-    x,
-    reduction = "pca",
-    dims = 1:d
-  )
-  str_section_head("Reduced") # logging
-  x <- FindNeighbors(
-    x,
-    reduction = "pca",
-    dims = 1:d
-  )
-  x <- FindClusters(
-    x,
-    resolution = 0.6
-  )
-  str_section_head("Clustered") # logging
-}
+d <- params["dims", ]
+x <- FindIntegrationAnchors(
+  object.list = objects,
+  dims = 1:d
+)
+x <- IntegrateData(
+  anchorset = x,
+  dims = 1:d
+)
+DefaultAssay(x) <- "integrated"
+str_section_head("Integrated") # logging
+all.genes <- rownames(x)
+x <- ScaleData(
+  x,
+  verbose = FALSE,
+  features = all.genes
+)
+x <- RunPCA(
+  x,
+  npcs = d,
+  verbose = FALSE
+)
+x <- RunUMAP(
+  x,
+  reduction = "pca",
+  dims = 1:d
+)
+str_section_head("Reduced") # logging
+x <- FindNeighbors(
+  x,
+  reduction = "pca",
+  dims = 1:d
+)
+x <- FindClusters(
+  x,
+  resolution = 0.6
+)
+str_section_head("Clustered") # logging
 
 save_object(x, "combined_integrated")
 
