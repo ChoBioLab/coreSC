eeef20ca004ab09dee6f62c82f826b2224758fe5
Christopher Tastad <ctastad@gmail.com>
2021-08-24 19:27:09 -0500

    added dimensionality plotting

--- scripts/integrate.R
+++ scripts/integrate.R
@@ -11,40 +11,71 @@ x <- FindIntegrationAnchors(
   object.list = objects,
   dims = 1:d
 )
+
 x <- IntegrateData(
   anchorset = x,
   dims = 1:d
 )
+
 DefaultAssay(x) <- "integrated"
+
 str_section_noloop("Integrated") # logging
+
 all.genes <- rownames(x)
+
 x <- ScaleData(
   x,
   verbose = FALSE,
   features = all.genes
 )
+
 x <- RunPCA(
   x,
   npcs = d,
   verbose = FALSE
 )
+
+x <- JackStraw(x)
+
+x <- ScoreJackStraw(
+  x,
+  dims = 1:d
+)
+
+plot1 <- JackStrawPlot(
+  x,
+  dims = 1:d
+)
+
+plot2 <- ElbowPlot(x)
+
+save_figure(
+  (plot1 + plot2),
+  "dimensionality",
+  width = 12,
+  height = 6
+)
+
 x <- RunUMAP(
   x,
   reduction = "pca",
   dims = 1:d
 )
+
 str_section_noloop("Reduced") # logging
+
 x <- FindNeighbors(
   x,
   reduction = "pca",
   dims = 1:d
 )
+
 x <- FindClusters(
   x,
   resolution = params["res", ]
 )
+
 str_section_noloop("Clustered") # logging
 
 save_object(x, "combined_integrated")
-
 print("End of integrate.R")
